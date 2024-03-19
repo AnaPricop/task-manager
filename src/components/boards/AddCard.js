83642e3651e469b4b9445d849305128855cfef80
Pricop Ana <pricop.ana17@gmail.com>
2024-03-19 17:46:57 +0200

    Add card changes

--- src/components/boards/AddCard.js
+++ src/components/boards/AddCard.js
@@ -23,7 +23,7 @@ const AddCard = ({setClicked, color}) => {
     const [taskData, setTaskData] = useState({
         title: "",
         description: "",
-        subject: "",
+        subject: [],
         dueDate: ""
     });
     const [tasks, setTasks] = useState([]);
@@ -32,51 +32,85 @@ const AddCard = ({setClicked, color}) => {
     };
     const onSubmit = (e) => {
         e.preventDefault();
-        axios
-            .post("http://localhost:8001/api/tasks", taskData)
-            .then((res) => {
-                setTaskData({
-                    title: "",
-                    description: "",
-                    subject: "",
-                    dueDate: ""
-                });
-                setTasks([...tasks, res.data.task]);
-                // setImage('./bck4.svg');
-                // handleCreate();
-            })
-            .catch((err) => {
-                console.log("Error in Create project!");
-            });
+        console.log(taskData)
+        // axios
+        //     .post("http://localhost:8001/api/tasks", taskData)
+        //     .then((res) => {
+        //         setTaskData({
+        //             title: "",
+        //             description: "",
+        //             subject: "",
+        //             dueDate: ""
+        //         });
+        //         setTasks([...tasks, res.data.task]);
+        //         // setImage('./bck4.svg');
+        //         // handleCreate();
+        //     })
+        //     .catch((err) => {
+        //         console.log("Error in Create project!");
+        //     });
     };
+    const [tags, setTags] = useState([
+        // "Daily task"
+    ]);
+    function handleKeyDown(e){
+        // If user did not press enter key, return
+        if(e.key !== 'Enter') return
+        // Get the value of the input
+        const value = e.target.value
+        // If the value is empty, return
+        if(!value.trim()) return
+        // Add the value to the tags array
+        setTags([...tags, value])
+        // Clear the input
+        e.target.value = ''
+        taskData.subject = tags;
+    }
+    function removeTag(index){
+        setTags(tags.filter((el, i) => i !== index))
+    }
     return (
         // <div className="col-3 mx-5">
         // <div className="task-status">To Do</div>
         <div className="add-card justify-content-center align-items-center">
             <form className="form-create-task" onSubmit={onSubmit}>
                 <span className="create-task">Create Task</span>
-                <div className="form-group">
-                    <label htmlFor="titleOfTask">Title</label>
+                <div className="form-group py-1">
+                    <label className='row px-2' htmlFor="titleOfTask">Title</label>
                     <input type="text" className="form-control" id="titleOfTask"
                            placeholder="" value={taskData.title} name="title"
                            onChange={onChange}/>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="taskDescription">Description</label>
+                <div className="form-group py-1">
+                    <label className='row px-2' htmlFor="taskDescription">Description</label>
                     <textarea type="text" className="form-control" id="taskDescription"
                            placeholder="What is this task about?" value={taskData.description} name="description"
                            onChange={onChange}/>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="taskDueDate">Due Date</label>
+                <div className="form-group py-1">
+                    <label className='row px-2' htmlFor="taskDueDate">Due Date</label>
                     <DatePicker
                         id="taskDueDate"
+                        style={{height: '30px'}}
                         // selected={date}
                         // onChange={handleDateChange}
                         showTimeSelect
                         dateFormat="Pp"
-                        selected={startDate} onChange={(date) => setStartDate(date)}
+                        selected={startDate} onChange={(date) => {setStartDate(date); taskData.dueDate = date.getUTCDate()  }}
+                        // value={taskData.description}
                     />
+                </div>
+                <div className="form-group py-1">
+                    <label className='row px-2'>Labels</label>
+                    <div className="tags-input-container">
+                        { tags.map((tag, index) => (
+                            <div className="tag-item" key={index}>
+                                <span className="text">{tag}</span>
+                                <span className="close" onClick={() => removeTag(index)}>&times;</span>
+                            </div>
+                        )) }
+                        <input onKeyDown={handleKeyDown} type="text" className="tags-input" placeholder="What is the subject of the task?" />
+                    </div>
                 </div>
                 <button type="submit" className={color ? color + " btn create-task-btn btn-sm" : " btn create-task-btn btn-sm"} >Create
                 </button>
